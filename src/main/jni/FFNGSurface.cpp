fcc20876109a257d2a7aa7e5011e6d361276b41c
Vašek Potoček <vasek.potocek@post.cz>
2018-07-04 03:20:51 +0200

    Use texture clamping to avoid overflowing wave effect

--- src/main/jni/FFNGSurface.cpp
+++ src/main/jni/FFNGSurface.cpp
@@ -31,6 +31,8 @@ SDL_Surface::SDL_Surface() : width(0), height(0) {
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 SDL_Surface::SDL_Surface(int width_, int height_, Uint32, bool) : SDL_Surface() {
