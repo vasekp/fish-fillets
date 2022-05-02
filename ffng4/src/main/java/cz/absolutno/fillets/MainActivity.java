455dd62f112a71557aac8cb04492104dcb77b70a
Vašek Potoček <vasek.potocek@post.cz>
2022-05-02 15:11:35 +0200

    Also hide UI in MainActivity

--- ffng4/src/main/java/cz/absolutno/fillets/MainActivity.java
+++ ffng4/src/main/java/cz/absolutno/fillets/MainActivity.java
@@ -1,12 +1,14 @@
 package cz.absolutno.fillets;
 
 import android.app.NativeActivity;
-import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,8 +20,15 @@ public class MainActivity extends NativeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, VideoActivity.class);
-        startActivity(intent);
+        if (Build.VERSION.SDK_INT >= 30) {
+            getWindow().getInsetsController().hide(
+                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        /*Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);*/
     }
 
     Bitmap loadBitmap(String filename) {
