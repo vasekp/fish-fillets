vasekp/fish-fillets

commit eb7297c2366a93a8741033b394f2abe072d6d37c

Wavy subtitles!

ffng4/src/main/assets/shader/wavy-text.frag
@@ -4,10 +4,13 @@ uniform sampler2D uSrcTexture;
 uniform vec2 uSrcSize;
 uniform vec4 uColor1;
 uniform vec4 uColor2;
+uniform float uTime;
 
 varying vec2 vPixCoords;
 
 void main(void)
 {
-    gl_FragColor = texture2D(uSrcTexture, vPixCoords / uSrcSize) * mix(uColor1, uColor2, smoothstep(0.3, 0.8, vPixCoords.y / uSrcSize.y));
+    float p = min(uTime - vPixCoords.x / uSrcSize.x, 1.0);
+    float dy = (1.0 - p) * cos(3.5 * 3.1416 * p) * .5;
+    gl_FragColor = texture2D(uSrcTexture, vPixCoords / uSrcSize - vec2(0.0, 1.0 + dy)) * mix(uColor1, uColor2, smoothstep(0.3, 0.8, vPixCoords.y / uSrcSize.y));
 }

ffng4/src/main/cpp/game/subtitles.cpp
@@ -31,7 +31,7 @@ void Subtitles::add(const std::string &text, const std::string& colors, float ad
     auto lines = breakLines(text);
     auto countLines = lines.size();
     for(auto& line : lines) {
-        float displayTime = std::max((float)text.length() * timePerChar, minTimePerLine);
+        float duration = std::max((float)text.length() * timePerChar, minTimePerLine);
         auto [color1, color2] = [&]() {
             if(m_colors.contains(colors))
                 return m_colors.at(colors);
@@ -40,7 +40,7 @@ void Subtitles::add(const std::string &text, const std::string& colors, float ad
                 return std::pair{Color::white, Color::white};
             }
         }();
-        m_lines.push_back({line, m_instance.graphics().renderLine(line), false, 0.f, displayTime,
+        m_lines.push_back({line, m_instance.graphics().renderLine(line), false, 0.f, 0.f, duration,
                            (unsigned) countLines, color1, color2});
     }
 }
@@ -59,11 +59,11 @@ void Subtitles::draw(const DrawTarget &target, float dTime, float absTime) {
         auto& line = *liveEnd;
         line.live = true;
         line.yOffset = -1.f;
-        line.hideTime += absTime;
+        line.addTime = absTime;
     }
     while(!m_lines.empty()) {
         const auto &front = m_lines.front();
-        if (front.live && (front.yOffset > 5 || front.hideTime < absTime))
+        if (front.live && (front.yOffset > 5 || absTime - front.addTime > front.duration))
             m_lines.erase(m_lines.begin(), m_lines.begin() + (int) m_lines.front().groupSize);
         else
             break;
@@ -73,9 +73,11 @@ void Subtitles::draw(const DrawTarget &target, float dTime, float absTime) {
             glUseProgram(textProgram);
             glUniform4fv(textProgram.uniform("uColor1"), 1, line.color1.gl().get());
             glUniform4fv(textProgram.uniform("uColor2"), 1, line.color2.gl().get());
-            float destY = pixelSize.fy() - (float)line.texture.height() * (1.f + line.yOffset);
-            target.blit(line.texture, textProgram, 0, (int)destY, 0, 0, DrawTarget::fullSize,
-                        DrawTarget::fullSize, pixelSize.x(), pixelSize.y());
+            glUniform1f(textProgram.uniform("uTime"), absTime - line.addTime);
+            auto height = line.texture.height();
+            float destY = pixelSize.fy() - (float)height * (1.f + line.yOffset);
+            target.blit(line.texture, textProgram, 0, (int)destY - (int)height, 0, 0, DrawTarget::fullSize,
+                        3 * height, pixelSize.x(), pixelSize.y());
         }
 }
 

ffng4/src/main/cpp/game/subtitles.h
@@ -9,7 +9,8 @@ class Subtitles {
         ogl::Texture texture;
         bool live;
         float yOffset;
-        float hideTime;
+        float addTime;
+        float duration;
         unsigned groupSize;
         Color color1;
         Color color2;

commit c296faf8834ff9a60941bba807eb55a0a8d42cfb

Fix gradient subtitle regression after eb7297c

ffng4/src/main/assets/shader/wavy-text.frag
@@ -12,5 +12,6 @@ void main(void)
 {
     float p = min(uTime - vPixCoords.x / uSrcSize.x, 1.0);
     float dy = (1.0 - p) * cos(3.5 * 3.1416 * p) * .25;
-    gl_FragColor = texture2D(uSrcTexture, vPixCoords / uSrcSize - vec2(0.0, 1.0 + dy)) * mix(uColor1, uColor2, smoothstep(0.3, 0.8, vPixCoords.y / uSrcSize.y));
+    vec2 texCoords = vPixCoords / uSrcSize - vec2(0.0, 1.0 + dy);
+    gl_FragColor = texture2D(uSrcTexture, texCoords) * mix(uColor1, uColor2, smoothstep(0.3, 0.8, texCoords.y));
 }

ffng4/src/main/cpp/game/graphics/subtitles.cpp
@@ -31,18 +31,21 @@ void Subtitles::add(const std::string &text, const std::string& colors, float ad
     auto lines = breakLines(text);
     auto countLines = lines.size();
     for(auto& line : lines) {
-        float duration = std::max((float)text.length() * timePerChar, minTimePerLine);
-        auto [color1, color2] = [&]() {
-            if(m_colors.contains(colors))
+        float duration = std::max((float) text.length() * timePerChar, minTimePerLine);
+        auto[color1, color2] = [&]() {
+            if (m_colors.contains(colors))
                 return m_colors.at(colors);
             else {
                 LOGE("Unknown color: %s", colors.c_str());
                 return std::pair{Color::white, Color::white};
             }
         }();
-        m_lines.push_back({line, m_instance.graphics().renderText(line, "font/font_subtitle.ttf", 16.f, 2.f,
-                                                                  m_instance.graphics().windowTarget()), false, 0.f, 0.f, duration,
-                           (unsigned) countLines, color1, color2});
+        auto texture = m_instance.graphics().renderText(line, "font/font_subtitle.ttf", 16.f, 2.f,
+                                                        m_instance.graphics().windowTarget());
+        m_lines.push_back(
+                {line, std::move(texture),
+                 false, 0.f, 0.f, duration,
+                 (unsigned) countLines, color1, color2});
     }
 }
 