07b350327f2879ef43e13ec5d3953bf0f822cd15
Vašek Potoček <vasek.potocek@post.cz>
2022-05-21 00:53:43 +0200

    Split modelanim.h/.cpp

---
+++ ffng4/src/main/cpp/game/graphics/modelanim.cpp
@@ -0,0 +1,52 @@
+#include "modelanim.h"
+
+ModelAnim::Layer::Layer(const std::array<std::vector<const Image*>, 2>& set, int phase) : m_set(set), m_length(set[0].size()), m_phase(phase) { }
+
+void ModelAnim::Layer::advance() {
+    m_phase++;
+    if(m_phase >= m_length)
+        m_phase = 0;
+}
+
+const Image* ModelAnim::Layer::get(int dir) const {
+    return m_set[dir][m_phase];
+}
+
+void ModelAnim::add(const std::string& name, int dir, const Image* image) {
+    m_images[name][dir].push_back(image);
+}
+
+void ModelAnim::set(const std::string& name, int phase, bool running) {
+    if(name == m_curName && running && m_running)
+        return;
+    m_main.emplace(m_images[name], phase);
+    m_curName = name;
+    m_running = running;
+}
+
+void ModelAnim::setExtra(const std::string& name, int phase) {
+    if(!name.empty())
+        m_extra.emplace(m_images[name], phase);
+    else
+        removeExtra();
+}
+
+void ModelAnim::removeExtra() {
+    m_extra.reset();
+}
+
+std::vector<const Image*> ModelAnim::get(int dir) const {
+    assert(m_main);
+    if(m_extra)
+        return {m_main->get(dir), m_extra->get(dir)};
+    else
+        return {m_main->get(dir)};
+}
+
+void ModelAnim::update() {
+    if(!m_running)
+        return;
+    m_main->advance();
+    if(m_extra)
+        m_extra->advance();
+}
--- ffng4/src/main/cpp/game/graphics/modelanim.h
+++ ffng4/src/main/cpp/game/graphics/modelanim.h
@@ -7,20 +7,14 @@
 class ModelAnim {
     class Layer {
         const std::array<std::vector<const Image*>, 2>& m_set;
+        std::size_t m_length;
         int m_phase;
-        int m_length;
 
     public:
-        Layer(const std::array<std::vector<const Image*>, 2>& set, int phase) : m_set(set), m_phase(phase), m_length(set[0].size()) { }
+        Layer(const std::array<std::vector<const Image*>, 2>& set, int phase);
 
-        void advance() {
-            m_phase++;
-            if(m_phase >= m_length)
-                m_phase = 0;
-        }
-        const Image* get(int dir) const {
-            return m_set[dir][m_phase];
-        }
+        void advance();
+        const Image* get(int dir) const;
     };
 
     std::map<std::string, std::array<std::vector<const Image*>, 2>> m_images;
@@ -30,45 +24,13 @@ class ModelAnim {
     std::optional<Layer> m_extra;
 
 public:
-    void add(const std::string& name, int dir, const Image* image) {
-        m_images[name][dir].push_back(image);
-    }
+    void add(const std::string& name, int dir, const Image* image);
+    void set(const std::string& name, int phase, bool running);
+    void setExtra(const std::string& name, int phase);
+    void removeExtra();
 
-    void set(const std::string& name, int phase, bool running) {
-        if(name == m_curName && running && m_running)
-            return;
-        m_main.emplace(m_images[name], phase);
-        m_curName = name;
-        m_running = running;
-    }
-
-    void setExtra(const std::string& name, int phase) {
-        if(!name.empty())
-            m_extra.emplace(m_images[name], phase);
-        else
-            removeExtra();
-    }
-
-    void removeExtra() {
-        m_extra.reset();
-    }
-
-    std::vector<const Image*>
-    get(int dir) const {
-        assert(m_main);
-        if(m_extra)
-            return {m_main->get(dir), m_extra->get(dir)};
-        else
-            return {m_main->get(dir)};
-    }
-
-    void update() {
-        if(!m_running)
-            return;
-        m_main->advance();
-        if(m_extra)
-            m_extra->advance();
-    }
+    void update();
+    std::vector<const Image*> get(int dir) const;
 };
 
 #endif //FISH_FILLETS_MODELANIM_H
