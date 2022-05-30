4584294591f01f46448b1836e994d005d4d78ed2
Vašek Potoček <vasek.potocek@post.cz>
2022-05-30 22:37:08 +0200

    Support for 11025 Hz (reactor, engine)

--- ffng4/src/main/cpp/subsystem/audio.cpp
+++ ffng4/src/main/cpp/subsystem/audio.cpp
@@ -128,7 +128,7 @@ AudioSourceRef Audio::loadMusic(const std::string& filename, bool async) {
         source->setLoop(start, end);
     } else
         source->setLoop();
-    source->setVolume(0.3);
+    source->setVolume(0.3f);
     return source;
 }
 
@@ -136,6 +136,7 @@ void loadSoundAsync(const std::string &filename, std::promise<AudioSourceRef>& p
     auto asset = instance.files().system(filename).asset();
 
     off64_t start, length;
+    bool doubleSample;
     auto fd = AAsset_openFileDescriptor64(asset, &start, &length);
     AMediaExtractor *extractor = AMediaExtractor_new();
     if (AMediaExtractor_setDataSourceFd(extractor, fd, start, length) != AMEDIA_OK)
@@ -145,9 +146,16 @@ void loadSoundAsync(const std::string &filename, std::promise<AudioSourceRef>& p
     int32_t sampleRate;
     if (!AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_SAMPLE_RATE, &sampleRate))
         ::error("AMediaFormat_getInt32 failed (sample rate)");
-    /*if (sampleRate != 22050) // TODO
-        ::error("Bad sample rate.", "Unexpected audio sample rate (%s): %d", filename.c_str(),
-                sampleRate);*/
+    switch(sampleRate) {
+        case 22050:
+            doubleSample = false;
+            break;
+        case 11025:
+            doubleSample = true;
+            break;
+        default:
+            ::error("Bad sample rate.", "Unexpected audio sample rate (%s): %d", filename.c_str(), sampleRate);
+    }
 
     int32_t channelCount;
     if (!AMediaFormat_getInt32(format, AMEDIAFORMAT_KEY_CHANNEL_COUNT, &channelCount))
@@ -188,12 +196,10 @@ void loadSoundAsync(const std::string &filename, std::promise<AudioSourceRef>& p
             auto presentationTimeUs = AMediaExtractor_getSampleTime(extractor);
 
             if (sampleSize > 0) {
-                AMediaCodec_queueInputBuffer(codec, inIndex, 0, sampleSize, presentationTimeUs,
-                                             0);
+                AMediaCodec_queueInputBuffer(codec, inIndex, 0, sampleSize, presentationTimeUs, 0);
                 AMediaExtractor_advance(extractor);
             } else {
-                AMediaCodec_queueInputBuffer(codec, inIndex, 0, 0, presentationTimeUs,
-                                             AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM);
+                AMediaCodec_queueInputBuffer(codec, inIndex, 0, 0, presentationTimeUs, AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM);
                 extractorDone = true;
             }
         } else if (inIndex != AMEDIACODEC_INFO_TRY_AGAIN_LATER)
@@ -207,13 +213,20 @@ void loadSoundAsync(const std::string &filename, std::promise<AudioSourceRef>& p
 
             auto samplesRead = info.size / 2;
             std::size_t outBufferSize;
-            std::uint8_t *outBuffer = AMediaCodec_getOutputBuffer(codec, outIndex,
-                                                                  &outBufferSize);
+            std::uint8_t *outBuffer = AMediaCodec_getOutputBuffer(codec, outIndex, &outBufferSize);
             assert(curSample + samplesRead <= numSamples);
-            oboe::convertPcm16ToFloat(reinterpret_cast<std::int16_t *>(outBuffer),
-                                      &data[curSample], samplesRead);
-            curSample += samplesRead;
-            //std::memcpy(dataRaw.data() + prevSize, outBuffer, info.size);
+            oboe::convertPcm16ToFloat(reinterpret_cast<std::int16_t*>(outBuffer), &data[curSample], samplesRead);
+            if(doubleSample) {
+                for(auto i = samplesRead - 1; i >= 0; i--) {
+                    float curr = data[curSample + i];
+                    float prev = i > 0 ? data[curSample + i - 1] :
+                                 curSample > 0 ? data[curSample - 2] : 0.f;
+                    data[curSample + 2 * i] = curr;
+                    data[curSample + 2 * i - 1] = (curr + prev) / 2.f;
+                }
+                curSample += 2 * samplesRead;
+            } else
+                curSample += samplesRead;
             AMediaCodec_releaseOutputBuffer(codec, outIndex, false);
         } else
             switch (outIndex) {
@@ -226,10 +239,10 @@ void loadSoundAsync(const std::string &filename, std::promise<AudioSourceRef>& p
                 case AMEDIACODEC_INFO_OUTPUT_BUFFERS_CHANGED:
                     break;
                 default:
-                    ::error("AMediaCodec_dequeueOutputBuffer failed", "outIndex = %d",
-                            outIndex);
+                    ::error("AMediaCodec_dequeueOutputBuffer failed", "outIndex = %d", outIndex);
             }
     } while (!(extractorDone && codecDone));
+    numSamples = curSample;
     LOGD("loadSound %s: decoded %d frames", filename.c_str(), (int)curSample);
 
     AMediaFormat_delete(format);
