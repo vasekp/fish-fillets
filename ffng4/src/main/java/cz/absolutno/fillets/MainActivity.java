d6096be4dbc5f054b2631a392631d9347df8a2ab
Vašek Potoček <vasek.potocek@post.cz>
2022-05-30 21:28:04 +0200

    Font adjustments

--- ffng4/src/main/java/cz/absolutno/fillets/MainActivity.java
+++ ffng4/src/main/java/cz/absolutno/fillets/MainActivity.java
@@ -108,6 +108,7 @@ public class MainActivity extends NativeActivity {
         Paint.FontMetrics fm = fillPaint.getFontMetrics();
         float top = Math.max(-fm.top, -fm.ascent);
         float bottom = Math.max(fm.bottom, fm.descent);
+        Log.d("FFNG", "fontSize " + fontSize + " top " + top + " bottom " + bottom);
         float lineHeight = top + bottom;
         Rect bounds = new Rect();
         fillPaint.getTextBounds(text, 0, text.length(), bounds);
