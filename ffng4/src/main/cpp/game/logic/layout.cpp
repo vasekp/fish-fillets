b469eebb56fdfef8ba30bb6143b2ea63fbee3c47
Vašek Potoček <vasek.potocek@post.cz>
2022-06-08 22:56:49 +0200

    Avoid touching border during GoTo

--- ffng4/src/main/cpp/game/logic/layout.cpp
+++ ffng4/src/main/cpp/game/logic/layout.cpp
@@ -127,6 +127,11 @@ std::vector<Direction> LevelLayout::findPath(const Model* unit, ICoords target) 
             for(auto dy = 0u; dy < model->shape().height(); dy++)
                 occupied[y + dy][x + dx] = model->shape()[dy][dx];
     }
+    /* Also avoid borders */
+    for(auto x = 0u; x < width(); x++)
+        occupied[0][x] = occupied[height() - 1][x] = true;
+    for(auto y = 0u; y < height(); y++)
+        occupied[y][0] = occupied[y][width() - 1] = true;
     /* Extend occupied fields to the left and above, as unit covers [x, x+unitWidth) × [y, y + unitHeight)
      * and thus can't start where it could clash with an occupied field due to its nonunit size */
     auto unitWidth = unit->shape().width();
