0ae537764a92a777b92bf16aea846ae4b49fe166
Vašek Potoček <vasek.potocek@post.cz>
2022-06-09 11:11:01 +0200

    Linux SystemFile (WIP: hardcoded path)

--- ffng4/src/main/cpp/platform/linux/files.cpp
+++ ffng4/src/main/cpp/platform/linux/files.cpp
@@ -2,7 +2,7 @@
 #include "./files.h"
 
 SystemFile Files::system(const std::string& path) const {
-    return {path, ""};
+    return {path, "/home/vasek/prog/android/fish-fillets/ffng4/src/main/assets/"};
 }
 
 UserFile Files::user(const std::string& path) const {
--- ffng4/src/main/cpp/platform/linux/files/system.cpp
+++ ffng4/src/main/cpp/platform/linux/files/system.cpp
@@ -4,15 +4,24 @@
 #include <fstream>
 
 SystemFile::SystemFile(std::string path, const std::filesystem::path& basePath) :
-        m_relPath(std::move(path)),
-        m_fullPath(basePath / m_relPath)
-{ }
+        m_relPath(std::move(path))
+{
+    auto overrideRel = "override" / m_relPath;
+    auto overrideFull = basePath / overrideRel;
+    if(std::filesystem::exists(overrideFull)) {
+        Log::debug("override ", m_relPath, " -> ", overrideRel);
+        m_fullPath = overrideFull;
+    } else
+        m_fullPath = basePath / m_relPath;
+}
 
 bool SystemFile::exists() const {
     return std::filesystem::exists(m_fullPath);
 }
 
 std::string SystemFile::read() const {
+    if(!exists())
+        Log::fatal("System file not found: ", m_fullPath);
     std::ostringstream oss;
     std::ifstream ifs{m_fullPath};
     oss << ifs.rdbuf();
