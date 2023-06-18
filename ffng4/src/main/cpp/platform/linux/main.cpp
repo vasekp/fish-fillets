418018e381409d649669e380b073325f33a3b150
Vašek Potoček <vasek.potocek@post.cz>
2023-06-18 21:27:57 +0200

    Linux: easier debugging

--- ffng4/src/main/cpp/platform/linux/main.cpp
+++ ffng4/src/main/cpp/platform/linux/main.cpp
@@ -9,53 +9,55 @@
 #include <X11/Xutil.h>
 #include <X11/XKBlib.h>
 
+#include <iostream>
+
 int main(int argc, char **argv) {
+    Display* dpy = XOpenDisplay(nullptr);
+    if(!dpy)
+        std::cerr << "cannot connect to X server";
+    Window root = DefaultRootWindow(dpy);
+
+    XSetWindowAttributes swa;
+    swa.event_mask = ButtonPressMask | ButtonReleaseMask | PointerMotionMask 
+        |
+        KeyPressMask | KeyReleaseMask | StructureNotifyMask;
+    Window win = XCreateWindow(dpy, root, 0, 0, 600, 600, 0,
+            CopyFromParent, InputOutput, CopyFromParent, CWEventMask, 
+            &swa);
+
+    XSetWindowAttributes xattr;
+    xattr.override_redirect = False;
+    XChangeWindowAttributes(dpy, win, CWOverrideRedirect, &xattr);
+
+    int one = 1;
+    XChangeProperty(dpy, win,
+            XInternAtom(dpy, "_HILDON_NON_COMPOSITED_WINDOW", False),
+            XA_INTEGER, 32, PropModeReplace, (unsigned char*)&one,  1);
+
+    XWMHints hints;
+    hints.input = True;
+    hints.flags = InputHint;
+    XSetWMHints(dpy, win, &hints);
+
+    auto wmDeleteMessage = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(dpy, win, &wmDeleteMessage, 1);
+
+    XkbSetDetectableAutoRepeat(dpy, True, nullptr);
+
+    XInstance instance{win};
+    AlsaSink sink{instance.audio()};
+
+    instance.graphics().activate();
+    instance.screens().startMode(ScreenManager::Mode::WorldMap);
+    instance.screens().drawFrame();
+
+    XMapWindow(dpy, win);
+    XFlush(dpy);
+
+    instance.screens().resume();
+    instance.running = true;
+
     try {
-        Display* dpy = XOpenDisplay(nullptr);
-        if(!dpy)
-            Log::fatal("cannot connect to X server");
-        Window root = DefaultRootWindow(dpy);
-
-        XSetWindowAttributes swa;
-        swa.event_mask = ButtonPressMask | ButtonReleaseMask | PointerMotionMask 
-            |
-            KeyPressMask | KeyReleaseMask | StructureNotifyMask;
-        Window win = XCreateWindow(dpy, root, 0, 0, 600, 600, 0,
-                CopyFromParent, InputOutput, CopyFromParent, CWEventMask, 
-                &swa);
-
-        XSetWindowAttributes xattr;
-        xattr.override_redirect = False;
-        XChangeWindowAttributes(dpy, win, CWOverrideRedirect, &xattr);
-
-        int one = 1;
-        XChangeProperty(dpy, win,
-                XInternAtom(dpy, "_HILDON_NON_COMPOSITED_WINDOW", False),
-                XA_INTEGER, 32, PropModeReplace, (unsigned char*)&one,  1);
-
-        XWMHints hints;
-        hints.input = True;
-        hints.flags = InputHint;
-        XSetWMHints(dpy, win, &hints);
-
-        auto wmDeleteMessage = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-        XSetWMProtocols(dpy, win, &wmDeleteMessage, 1);
-
-        XkbSetDetectableAutoRepeat(dpy, True, nullptr);
-
-        XInstance instance{win};
-        AlsaSink sink{instance.audio()};
-
-        instance.graphics().activate();
-        instance.screens().startMode(ScreenManager::Mode::WorldMap);
-        instance.screens().drawFrame();
-
-        XMapWindow(dpy, win);
-        XFlush(dpy);
-
-        instance.screens().resume();
-        instance.running = true;
-
         Log::info("Main loop");
         int lastWidth = 0, lastHeight = 0;
         while(instance.running) {
@@ -98,7 +100,6 @@ int main(int argc, char **argv) {
             instance.inputSource().ping();
             instance.screens().drawFrame();
         }
-
     } catch(std::exception& e) {
         Log::error("Caught exception ", e.what(), ", exiting");
         return 1;
