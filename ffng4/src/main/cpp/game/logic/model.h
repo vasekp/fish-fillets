vasekp/fish-fillets

commit 750b98fcfb09a1f3c1a889837e9786bce2f69a1c

Bonus level: finish

ffng4/src/main/cpp/game/logic/model.h
@@ -21,6 +21,7 @@ public:
         item_heavy,
         fish_old_small,
         fish_old_big,
+        bonus_box,
         bonus_exit,
         wall,
         virt
@@ -83,6 +84,7 @@ private:
     bool m_alive;
     bool m_pushing;
     bool m_driven;
+    bool m_hidden;
     Action m_action;
     Orientation m_orientation;
     ModelAnim m_anim;
@@ -118,8 +120,9 @@ public:
 
     bool alive() const { return m_alive; }
     bool talking() const { return m_talk && !m_talk->done(); }
-    bool isVirtual() const { return m_type == Type::virt; }
-    bool movable() const { return (m_type == Type::item_light || m_type == Type::item_heavy) && !m_driven; }
+    bool isVirtual() const { return m_type == Type::virt || m_hidden; }
+    bool hidden() const { return m_type == Type::virt || m_hidden; }
+    bool movable() const { return m_supportType == SupportType::none && !m_driven; }
     bool moving() const { return (bool)m_move; }
     bool pushing() const { return m_move && m_pushing; }
     bool falling() const { return !alive() && m_move == Direction::down; }
@@ -133,7 +136,7 @@ public:
     auto& driven() { return m_driven; }
     auto driven() const { return m_driven; }
 
-    bool intersects(Model* other, ICoords d) const;
+    bool intersects(Model* other, Direction d = Direction::none) const;
 
     void turn();
     void displace(ICoords d, bool pushing = false);
@@ -143,7 +146,7 @@ public:
     void deltaStop();
     void die();
     void bonusSwitch(bool value);
-    void disappear() { m_type = Type::virt; }
+    void disappear() { m_hidden = true; }
     AudioSource::Ref& talk() { return m_talk; }
     void setEffect(const std::string& name, float startTime);
     auto effect() const { return m_effect; }

ffng4/src/main/cpp/game/logic/rules.cpp
@@ -8,7 +8,8 @@ LevelRules::LevelRules(Level &level, LevelLayout &layout) :
     m_layout(layout),
     m_curFish(nullptr),
     m_doomed(false),
-    m_vintage(false)
+    m_vintage(false),
+    m_bonusExit(nullptr)
 {
     m_small = *std::find_if(m_layout.models().begin(), m_layout.models().end(), [](const auto& model) { return model->type() == Model::Type::fish_small; });
     m_big = *std::find_if(m_layout.models().begin(), m_layout.models().end(), [](const auto& model) { return model->type() == Model::Type::fish_big; });
@@ -112,7 +113,7 @@ void LevelRules::processKey(Key key) {
 
 bool LevelRules::switchFish(Model* which) {
     Model* target = which != nullptr ? which : m_curFish == m_small ? m_big : m_small;
-    if(target->action() == Model::Action::busy || !target->alive() || m_layout.borderDepth(target).first > 0)
+    if(target->action() == Model::Action::busy || !target->alive() || m_layout.borderDepth(target).first > 0 || target->hidden())
         return false;
     setFish(target);
     if(!m_vintage) {
@@ -125,9 +126,14 @@ bool LevelRules::switchFish(Model* which) {
 }
 
 void LevelRules::bonusSwitch(bool value, bool keepQueue) {
-    auto* plug = *std::find_if(m_layout.models().begin(), m_layout.models().end(), [](const auto& model) { return model->type() == Model::Type::bonus_exit; });
-    plug->bonusSwitch(value);
-    updateDepGraph(plug);
+    std::for_each(m_layout.models().begin(), m_layout.models().end(), [&, value](auto* model) {
+            if(auto type = model->type(); type == Model::Type::bonus_box || type == Model::Type::bonus_exit) {
+                model->bonusSwitch(value);
+                updateDepGraph(model);
+                if(type == Model::Type::bonus_exit)
+                    m_bonusExit = model;
+            }
+    });
     m_small = *std::find_if(m_layout.models().begin(), m_layout.models().end(), [type = value ? Model::Type::fish_old_small : Model::Type::fish_small](const auto& model) { return model->type() == type; });
     m_big = *std::find_if(m_layout.models().begin(), m_layout.models().end(), [type = value ? Model::Type::fish_old_big : Model::Type::fish_big](const auto& model) { return model->type() == type; });
     setFish(Model::Fish::small);
@@ -144,6 +150,7 @@ void LevelRules::moveFish(Model::Fish which, Direction d) {
 void LevelRules::moveFish(Direction d) {
     if(!m_curFish->alive() || m_curFish->action() == Model::Action::busy || m_curFish->driven())
         return;
+
     if((m_curFish->orientation() == Model::Orientation::right && d.x < 0) ||
        (m_curFish->orientation() == Model::Orientation::left && d.x > 0)) {
         if(!m_vintage) {
@@ -158,6 +165,9 @@ void LevelRules::moveFish(Direction d) {
             moveFish(d);
         }
         return;
+    }
+
+    if(m_vintage) {
     }
 
     const auto obs = m_layout.obstacles(m_curFish, d);
@@ -344,13 +354,15 @@ void LevelRules::evalMotion(Model* model, Direction d) {
                 return true;
             }, true);
         }
-        if(depth.second >= 0) {
+        if(depth.second >= 0 || (m_bonusExit && model->intersects(m_bonusExit))) {
             std::erase(m_goals, model);
             if(model == m_curFish)
                 if(!switchFish()) {
                     setFish(Model::Fish::none);
-                    m_level.success();
+                    if(!m_vintage)
+                        m_level.success();
                 }
+            model->disappear();
         }
     }
 }

ffng4/src/main/assets/script/windoze/models.lua
@@ -68,7 +68,7 @@ XX
 ]])
 addItemAnim(item_light, "images/"..codename.."/icon1.png")
 
-bonuslevel = addModel("item_light", 23, 9,
+bonuslevel = addModel("bonus_box", 23, 9,
 [[
 XXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXX

ffng4/src/main/cpp/game/logic/interface.cpp
@@ -281,7 +281,7 @@ bool Level::model_isAtBorder(int index) {
 }
 
 bool Level::model_isOut(int index) {
-    return layout().borderDepth(layout().getModel(index)).second >= 0;
+    return layout().getModel(index)->hidden();
 }
 
 bool Level::model_isLeft(int index) {

ffng4/src/main/cpp/game/logic/model.cpp
@@ -1,27 +1,17 @@
 #include "model.h"
 
-std::tuple<Model::Type, bool, Model::SupportType, Model::Weight> decode(const std::string& type) {
-    if(type == "item_light")
-        return {Model::Type::item_light, false, Model::SupportType::none, Model::Weight::light};
-    else if(type == "item_heavy")
-        return {Model::Type::item_heavy, false, Model::SupportType::none, Model::Weight::heavy};
-    else if(type == "item_fixed")
-        return {Model::Type::wall, false, Model::SupportType::wall, Model::Weight::none};
-    else if(type == "fish_small")
-        return {Model::Type::fish_small, true, Model::SupportType::small, Model::Weight::none};
-    else if(type == "fish_big")
-        return {Model::Type::fish_big, true, Model::SupportType::big, Model::Weight::none};
-    else if(type == "fish_old_small")
-        return {Model::Type::fish_old_small, true, Model::SupportType::small, Model::Weight::none};
-    else if(type == "fish_old_big")
-        return {Model::Type::fish_old_big, true, Model::SupportType::big, Model::Weight::none};
-    else if(type == "bonus_exit")
-        return {Model::Type::bonus_exit, false, Model::SupportType::none, Model::Weight::none};
-    else if(type == "virtual")
-        return {Model::Type::virt, false, Model::SupportType::none, Model::Weight::none};
-    else
-        Log::fatal("Type not implemented: ", type);
-}
+static const std::map<std::string, std::tuple<Model::Type, bool, Model::SupportType, Model::Weight>> modelTypes{{
+    {"item_light", {Model::Type::item_light, false, Model::SupportType::none, Model::Weight::light}},
+    {"item_heavy", {Model::Type::item_heavy, false, Model::SupportType::none, Model::Weight::heavy}},
+    {"item_fixed", {Model::Type::wall, false, Model::SupportType::wall, Model::Weight::none}},
+    {"fish_small", {Model::Type::fish_small, true, Model::SupportType::small, Model::Weight::none}},
+    {"fish_big", {Model::Type::fish_big, true, Model::SupportType::big, Model::Weight::none}},
+    {"fish_old_small", {Model::Type::fish_old_small, true, Model::SupportType::small, Model::Weight::none}},
+    {"fish_old_big", {Model::Type::fish_old_big, true, Model::SupportType::big, Model::Weight::none}},
+    {"bonus_box", {Model::Type::bonus_box, false, Model::SupportType::none, Model::Weight::light}},
+    {"bonus_exit", {Model::Type::bonus_exit, false, Model::SupportType::none, Model::Weight::light}},
+    {"virtual", {Model::Type::virt, false, Model::SupportType::none, Model::Weight::none}},
+}};
 
 Model::Model(int index, const std::string& type, int x, int y, const std::string& shape) :
         m_index(index),
@@ -32,13 +22,14 @@ Model::Model(int index, const std::string& type, int x, int y, const std::string
         m_shape(shape),
         m_pushing(false),
         m_driven(false),
+        m_hidden(false),
         m_action(Action::base),
         m_orientation(Orientation::left),
         m_touchDir(),
         m_warp(1.f),
         m_effect{Effect::none, 0}
 {
-    std::tie(m_type, m_alive, m_supportType, m_weight) = decode(type);
+    std::tie(m_type, m_alive, m_supportType, m_weight) = modelTypes.at(type);
 }
 
 void Model::turn() {
@@ -93,7 +84,10 @@ void Model::die() {
 }
 
 void Model::bonusSwitch(bool value) {
-    m_supportType = value ? SupportType::wall : SupportType::none;
+    if(m_type == Type::bonus_box)
+        m_supportType = value ? SupportType::wall : SupportType::none;
+    if(m_type == Type::bonus_exit)
+        m_hidden = value;
 }
 
 void Model::deltaStop() {
@@ -108,7 +102,7 @@ FCoords Model::fxy() const {
         return m_position + m_viewShift;
 }
 
-bool Model::intersects(Model* other, ICoords d) const {
+bool Model::intersects(Model* other, Direction d) const {
     return shape().intersects(other->shape(), other->xy() - (xy() + d));
 }
 

ffng4/src/main/cpp/game/logic/rules.h
@@ -16,6 +16,7 @@ class LevelRules {
     Model* m_curFish;
     bool m_doomed;
     bool m_vintage;
+    Model* m_bonusExit;
 
     std::vector<Model*> m_goals;
 

commit 7bbddc1acb999e7b6caead8be8dc18dad556b6f4

Fix skeletons not falling down (error introduced in 750b98fc)

ffng4/src/main/cpp/game/logic/model.h
@@ -121,7 +121,7 @@ public:
     bool alive() const { return m_alive; }
     bool talking() const { return m_talk && !m_talk->done(); }
     bool hidden() const { return m_type == Type::virt || m_hidden; }
-    bool movable() const { return m_supportType == SupportType::none && !m_driven; }
+    bool movable() const { return (m_supportType == SupportType::none || m_supportType == SupportType::weak) && !m_driven; }
     bool moving() const { return (bool)m_move; }
     bool pushing() const { return m_move && m_pushing; }
     bool falling() const { return !alive() && m_move == Direction::down; }

ffng4/src/main/cpp/game/logic/rules.cpp
@@ -167,9 +167,6 @@ void LevelRules::moveFish(Direction d) {
         return;
     }
 
-    if(m_vintage) {
-    }
-
     const auto obs = m_layout.obstacles(m_curFish, d);
 
     if(m_layout.borderDepth(m_curFish, d).first > 0 && m_curFish->goal() != Model::Goal::escape)