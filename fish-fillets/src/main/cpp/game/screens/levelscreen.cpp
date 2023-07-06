vasekp/fish-fillets

commit 8f324b726fcaaa9d44d473b99e9fbeeef7e6d957

Simplify game action (save, load, restart) logic

fish-fillets/src/main/cpp/game/screens/levelscreen.cpp
@@ -11,7 +11,9 @@ LevelScreen::LevelScreen(Instance& instance, LevelRecord& record) :
         m_winSize(),
         m_subs(instance),
         m_flashAlpha(0)
-{ }
+{
+    m_level.init();
+}
 
 void LevelScreen::restore() {
     m_display.reset();
@@ -24,7 +26,6 @@ void LevelScreen::exit() {
 
 void LevelScreen::own_start() {
     m_instance.audio().clear();
-    m_level.init();
 }
 
 void LevelScreen::own_resize() {

fish-fillets/src/main/cpp/game/logic/level.cpp
@@ -180,39 +180,28 @@ void Level::save(bool force) {
 void Level::load(bool keepSchedule) {
     if(loadPossible() || keepSchedule) {
         killDialogsHard();
-        auto contents = saveFile()->read();
-        if(keepSchedule) {
-            reinit(true);
-            m_script.doString(contents);
-            m_script.doString("script_load()");
-        } else {
+        if(!keepSchedule)
             m_tickSchedule.clear();
-            m_tickSchedule.emplace_back([&, contents]() {
-                reinit();
-                m_script.doString(contents);
-                m_script.doString("script_load()");
-                return true;
-            });
-        }
+        reinit(keepSchedule);
+        m_script.doString(saveFile()->read());
+        m_script.doString("script_load()");
     }
 }
 
 void Level::success() {
     m_tickSchedule.emplace_back([&]() {
-        if(m_tickSchedule.size() == 1 && !m_instance.audio().isDialog()) {
-            if(!isBusy(BusyReason::replay)) {
-                m_record.solved = true;
-                solveFile()->write("saved_moves = '"s + m_replay + "'\n");
-            }
-            if(record().script_ending.empty())
-                m_screen.exit();
-            else {
-                m_script.loadFile(record().script_ending);
-                setBusy(BusyReason::poster);
-            }
+        if(m_instance.audio().isDialog())
+            return false;
+        if(!isBusy(BusyReason::replay)) {
+            m_record.solved = true;
+            solveFile()->write("saved_moves = '"s + m_replay + "'\n");
         }
-        else
-            m_tickSchedule.push_back(std::move(m_tickSchedule.front()));
+        if(record().script_ending.empty())
+            m_screen.exit();
+        else {
+            m_script.loadFile(record().script_ending);
+            setBusy(BusyReason::poster);
+        }
         return true;
     });
 }
@@ -220,12 +209,9 @@ void Level::success() {
 void Level::replay() {
     input().setRestartPossible(false);
     setBusy(BusyReason::replay);
-    m_tickSchedule.emplace_back([&, contents = solveFile()->read()]() {
-        m_script.doString(contents);
-        m_script.doString("saved_models = {{}}");
-        m_script.doString("script_load()");
-        return true;
-    });
+    m_script.doString(solveFile()->read());
+    m_script.doString("saved_models = {{}}");
+    m_script.doString("script_load()");
 }
 
 bool Level::inReplay() const {
@@ -234,27 +220,18 @@ bool Level::inReplay() const {
 
 void Level::restart(bool keepSchedule) {
     killDialogsHard();
-    if(keepSchedule) {
-        reinit(true);
-    } else {
-        m_tickSchedule.clear(); // TODO potřebujeme tick?
-        m_tickSchedule.emplace_back([&, keepSchedule]() {
-            reinit(keepSchedule);
-            return true;
-        });
-    }
+    if(!keepSchedule)
+        m_tickSchedule.clear();
+    reinit(keepSchedule);
 }
 
 void Level::restartWhenEmpty() {
     m_tickSchedule.emplace_back([&]() {
-        Log::debug<Log::lua>("restartWhenEmpty waiting callbacks: ", m_tickSchedule.size());
-        if(m_tickSchedule.size() == 1 && !m_instance.audio().isDialog()) {
-            m_screen.subs().clear();
-            m_screen.killSounds();
-            reinit();
-        }
-        else
-            m_tickSchedule.push_back(std::move(m_tickSchedule.front()));
+        if(m_instance.audio().isDialog())
+            return false;
+        m_screen.subs().clear();
+        m_screen.killSounds();
+        reinit();
         return true;
     });
 }

commit e6d64ea747c09df29c140b1aa01a823f79886a9f

Fix rooms not playing music (caused by 8f324b72)

fish-fillets/src/main/cpp/game/screens/levelscreen.cpp
@@ -3,17 +3,16 @@
 #include "screenmanager.h"
 #include "subsystem/rng.h"
 
-LevelScreen::LevelScreen(Instance& instance, LevelRecord& record) :
+LevelScreen::LevelScreen(Instance& instance, LevelRecord& record, bool replay) :
         GameScreen(instance),
         m_level(instance, *this, record),
         m_input(instance, *this),
         m_waves(),
         m_winSize(),
         m_subs(instance),
-        m_flashAlpha(0)
-{
-    m_level.init();
-}
+        m_flashAlpha(0),
+        m_replay(replay)
+{ }
 
 void LevelScreen::reset() {
     m_display.reset();
@@ -28,6 +27,9 @@ void LevelScreen::exit() {
 
 void LevelScreen::own_start() {
     m_instance.audio().clear();
+    m_level.init();
+    if(m_replay)
+        m_level.replay();
 }
 
 void LevelScreen::own_resize() {

fish-fillets/src/main/cpp/game/screens/levelscreen.h
@@ -20,13 +20,13 @@ class LevelScreen : public GameScreen {
     Subtitles m_subs;
     std::optional<Hint> m_hint;
     std::optional<PNGImage> m_display;
-
     std::unique_ptr<TextureTarget> m_mirrorTarget;
     std::unique_ptr<ZXEffect> m_zxEffect;
     float m_flashAlpha;
+    bool m_replay;
 
 public:
-    LevelScreen(Instance&, LevelRecord&);
+    LevelScreen(Instance& instance, LevelRecord& record, bool replay);
 
     Level& level() { return m_level; }
     IInputSink& input() override { return m_input; }

fish-fillets/src/main/cpp/game/screens/screenmanager.cpp
@@ -40,9 +40,7 @@ void ScreenManager::announceLevel(const std::string& title) {
 }
 
 void ScreenManager::startLevel(LevelRecord& record, bool replay) {
-    auto& level = open<LevelScreen>(record).level();
-    if(replay)
-        level.replay();
+    auto& level = open<LevelScreen>(record, replay).level();
     m_title.fadeout();
 }
 