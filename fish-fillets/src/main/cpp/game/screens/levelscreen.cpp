vasekp/fish-fillets

commit 3aafa25a65f40de8620d9e0a712139f9e44cc898

Merge drawOverlays() into draw()

fish-fillets/src/main/cpp/game/screens/levelscreen.cpp
@@ -157,6 +157,11 @@ void LevelScreen::own_draw(const DrawTarget& target, float dt) {
         target.fill(coords, flatProgram, 0, 0, m_winSize.fx(), m_winSize.fy());
         m_flashAlpha = std::max(m_flashAlpha - flashDecay * dt, 0.f);
     }
+
+    m_subs.draw(target, dt, timeAlive());
+    if(m_hint)
+        m_hint->draw(target);
+    m_input.draw(target, timeAlive());
 }
 
 AudioData::Ref LevelScreen::addSound(const std::string &name, const std::string &filename, bool single) {
@@ -301,13 +306,6 @@ bool LevelScreen::own_key(Key key) {
         default:
             return false;
     }
-}
-
-void LevelScreen::own_drawOverlays(const DrawTarget &target, float dTime, float absTime) {
-    m_subs.draw(target, dTime, absTime);
-    if(m_hint)
-        m_hint->draw(target);
-    m_input.draw(target, absTime);
 }
 
 void LevelScreen::display(const std::string& filename) {

fish-fillets/src/main/cpp/game/screens/levelscreen.h
@@ -61,7 +61,6 @@ private:
     void own_pause() override;
     void own_resume() override;
     void own_draw(const DrawTarget& target, float dt) override;
-    void own_drawOverlays(const DrawTarget &target, float dTime, float absTime) override;
     bool own_key(Key key) override;
 
     std::unique_ptr<TextureTarget> makeMirrorTarget(const Model& model);

fish-fillets/src/main/cpp/game/screens/screen.cpp
@@ -2,7 +2,7 @@
 
 GameScreen::GameScreen(Instance &instance) :
     m_instance(instance),
-    m_lastDraw(0), m_lastDrawOverlays(0),
+    m_lastDraw(0),
     m_running(false)
 { }
 
@@ -69,11 +69,4 @@ void GameScreen::draw(const DrawTarget& target) {
     auto liveTime = timeAlive();
     own_draw(target, liveTime - m_lastDraw);
     m_lastDraw = liveTime;
-}
-
-void GameScreen::drawOverlays(const DrawTarget& target) {
-    target.bind();
-    auto liveTime = timeAlive();
-    own_drawOverlays(target, liveTime - m_lastDrawOverlays, liveTime); // TODO 3rd argument not needed
-    m_lastDrawOverlays = liveTime;
 }

fish-fillets/src/main/cpp/game/screens/screen.h
@@ -12,7 +12,6 @@ protected:
     std::chrono::steady_clock::time_point m_relStartTime;
     std::chrono::steady_clock::time_point m_pauseTime;
     float m_lastDraw;
-    float m_lastDrawOverlays;
     bool m_running;
 
 private:
@@ -28,7 +27,6 @@ public:
     void pause();
     void resume();
     void draw(const DrawTarget& target);
-    void drawOverlays(const DrawTarget& target);
     bool pointer(FCoords coords) { return own_pointer(coords); }
     bool keypress(Key key) { return own_key(key); }
 
@@ -46,7 +44,6 @@ protected:
     virtual void own_resume() { }
     virtual void own_resize();
     virtual void own_draw(const DrawTarget& target, float dt) = 0;
-    virtual void own_drawOverlays(const DrawTarget& target, float dTime, float absTime) { }
     virtual bool own_pointer(FCoords coords) { return false; }
     virtual bool own_key(Key key) { return false; }
 };

fish-fillets/src/main/cpp/game/screens/screenmanager.cpp
@@ -74,7 +74,6 @@ void ScreenManager::drawFrame() {
     glClear(GL_COLOR_BUFFER_BIT);
     curScreen().draw(offscreen);
 
-    curScreen().drawOverlays(offscreen);
     m_title.draw(offscreen);
 
     if(m_options.visible()) {

commit 46339c9621f19e51ca2fed0d8faf08f46c5d2cb2

Fix an error introduced in 3aafa25a

fish-fillets/src/main/cpp/game/screens/levelscreen.cpp
@@ -73,6 +73,23 @@ void LevelScreen::own_update() {
 }
 
 void LevelScreen::own_draw(const DrawTarget& target) {
+    drawLevel(target);
+
+    if(m_flashAlpha > 0) {
+        const auto& flatProgram = m_instance.graphics().shaders().flat;
+        const auto& coords = m_instance.graphics().coords(Graphics::CoordSystems::window);
+        glUseProgram(flatProgram);
+        glUniform4fv(flatProgram.uniform("uColor"), 1, Color::white.gl(m_flashAlpha).data());
+        target.fill(coords, flatProgram, 0, 0, m_winSize.fx(), m_winSize.fy());
+    }
+
+    m_subs.draw(target, timeAlive());
+    if(m_hint)
+        m_hint->draw(target);
+    m_input.draw(target);
+}
+
+void LevelScreen::drawLevel(const DrawTarget& target) {
     const auto& copyProgram = m_instance.graphics().shaders().copy;
     const auto& wavyProgram = m_instance.graphics().shaders().wavyImage;
     const auto& flatProgram = m_instance.graphics().shaders().flat;
@@ -164,17 +181,6 @@ void LevelScreen::own_draw(const DrawTarget& target) {
         m_instance.graphics().setMask(m_mirrorTarget->texture().texture()); // TODO
         target.blit(mirror->anim().get(mirror->orientation())[0], coords, m_instance.graphics().shaders().mirror, mirror->fx() * size_unit, mirror->fy() * size_unit);
     }
-
-    if(m_flashAlpha > 0) {
-        glUseProgram(flatProgram);
-        glUniform4fv(flatProgram.uniform("uColor"), 1, Color::white.gl(m_flashAlpha).data());
-        target.fill(coords, flatProgram, 0, 0, m_winSize.fx(), m_winSize.fy());
-    }
-
-    m_subs.draw(target, timeAlive());
-    if(m_hint)
-        m_hint->draw(target);
-    m_input.draw(target);
 }
 
 AudioData::Ref LevelScreen::addSound(const std::string &name, const std::string &filename, bool single) {

fish-fillets/src/main/cpp/game/screens/levelscreen.h
@@ -65,6 +65,8 @@ private:
     void own_draw(const DrawTarget& target) override;
     bool own_key(Key key) override;
 
+    void drawLevel(const DrawTarget& target);
+
     std::unique_ptr<TextureTarget> makeMirrorTarget(const Model& model);
 
     constexpr static float flashInit = 0.5f;