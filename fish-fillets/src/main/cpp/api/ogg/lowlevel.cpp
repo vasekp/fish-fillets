822f789464cd3d2a113da7cd4387df26856b8f1e
Vašek Potoček <vasek.potocek@post.cz>
2023-07-15 22:15:26 +0200

    C++ style: split api/ogg/{lowlevel.h / lowlevel.cpp}

---
+++ fish-fillets/src/main/cpp/api/ogg/lowlevel.cpp
@@ -0,0 +1,63 @@
+#include "lowlevel.h"
+
+namespace ogg::ll {
+
+    using namespace ogg::internal;
+
+    OggSync::OggSync(const std::string& data) {
+        operator<<(data);
+    }
+
+    void OggSync::operator<<(const std::string& data) {
+        auto buffer = ogg_sync_buffer(this, data.size());
+        std::memcpy(buffer, data.data(), data.size());
+        ogg_sync_wrote(this, data.size());
+    }
+
+    bool OggSync::operator>>(ogg_page& page) {
+        return ogg_sync_pageout(this, &page) == 1;
+    }
+
+    OggStream::OggStream(ogg_page& page) : OggStream(ogg_page_serialno(&page)) {
+        operator<<(page);
+    }
+
+    bool OggStream::operator<<(ogg_page& page) {
+        return ogg_stream_pagein(this, &page) == 0;
+    }
+
+    bool OggStream::operator>>(ogg_packet& packet) {
+        return ogg_stream_packetout(this, &packet) == 1;
+    }
+
+    VorbisDecoder::VorbisDecoder(vorbis_info* info) : OggStruct(info) {
+        vorbis_block_init(this, &m_block);
+    }
+
+    VorbisDecoder::~VorbisDecoder() {
+        vorbis_block_clear(&m_block);
+    }
+
+    void VorbisDecoder::operator<<(ogg_packet& packet) {
+        if(auto ret = vorbis_synthesis(&m_block, &packet); ret != 0) {
+            Log::error("vorbis_synthesis returned ", ret);
+            return;
+        }
+        if(auto ret = vorbis_synthesis_blockin(this, &m_block); ret != 0)
+            Log::error("vorbis_synthesis_blockin returned ", ret);
+    }
+
+    std::size_t VorbisDecoder::operator>>(std::vector<float>& ret) {
+        float **pcm;
+        auto oldSize = ret.size();
+        auto size = vorbis_synthesis_pcmout(this, &pcm);
+        Log::verbose<Log::video>("pcmout size: ", size, " vector ", (void*)(pcm), " ", (void*)(pcm[0]));
+        if(size == 0)
+            return 0;
+        ret.resize(oldSize + size);
+        std::memcpy(ret.data() + oldSize, pcm[0], size * sizeof(float));
+        vorbis_synthesis_read(this, size);
+        return size;
+    }
+
+}
--- fish-fillets/src/main/cpp/api/ogg/lowlevel.h
+++ fish-fillets/src/main/cpp/api/ogg/lowlevel.h
@@ -17,70 +17,30 @@ namespace ogg::ll {
 
     class OggSync : public OggStruct<ogg_sync_state, ogg_sync_init, ogg_sync_clear> {
     public:
-        OggSync(const std::string& data) {
-            operator<<(data);
-        }
+        OggSync(const std::string& data);
 
-        void operator<<(const std::string& data) {
-            auto buffer = ogg_sync_buffer(this, data.size());
-            std::memcpy(buffer, data.data(), data.size());
-            ogg_sync_wrote(this, data.size());
-        }
-
-        bool operator>>(ogg_page& page) {
-            return ogg_sync_pageout(this, &page) == 1;
-        }
+        void operator<<(const std::string& data);
+        bool operator>>(ogg_page& page);
     };
 
     class OggStream : public OggStruct<ogg_stream_state, ogg_stream_init, ogg_stream_clear> {
     public:
         using OggStruct::OggStruct;
 
-        OggStream(ogg_page& page) : OggStream(ogg_page_serialno(&page)) {
-            operator<<(page);
-        }
+        OggStream(ogg_page& page);
 
-        bool operator<<(ogg_page& page) {
-            return ogg_stream_pagein(this, &page) == 0;
-        }
-
-        bool operator>>(ogg_packet& packet) {
-            return ogg_stream_packetout(this, &packet) == 1;
-        }
+        bool operator<<(ogg_page& page);
+        bool operator>>(ogg_packet& packet);
     };
 
     class VorbisDecoder : public OggStruct<vorbis_dsp_state, vorbis_synthesis_init, vorbis_dsp_clear> {
         vorbis_block m_block;
     public:
-        VorbisDecoder(vorbis_info* info) : OggStruct(info) {
-            vorbis_block_init(this, &m_block);
-        }
+        VorbisDecoder(vorbis_info* info);
+        ~VorbisDecoder();
 
-        ~VorbisDecoder() {
-            vorbis_block_clear(&m_block);
-        }
-
-        void operator<<(ogg_packet& packet) {
-            if(auto ret = vorbis_synthesis(&m_block, &packet); ret != 0) {
-                Log::error("vorbis_synthesis returned ", ret);
-                return;
-            }
-            if(auto ret = vorbis_synthesis_blockin(this, &m_block); ret != 0)
-                Log::error("vorbis_synthesis_blockin returned ", ret);
-        }
-
-        std::size_t operator>>(std::vector<float>& ret) {
-            float **pcm;
-            auto oldSize = ret.size();
-            auto size = vorbis_synthesis_pcmout(this, &pcm);
-            Log::verbose<Log::video>("pcmout size: ", size, " vector ", (void*)(pcm), " ", (void*)(pcm[0]));
-            if(size == 0)
-                return 0;
-            ret.resize(oldSize + size);
-            std::memcpy(ret.data() + oldSize, pcm[0], size * sizeof(float));
-            vorbis_synthesis_read(this, size);
-            return size;
-        }
+        void operator<<(ogg_packet& packet);
+        std::size_t operator>>(std::vector<float>& ret);
     };
 
 }
