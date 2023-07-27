890f0a0f1ff6cf42dfb9d77646be7f3c17d98d0d
Vašek Potoček <vasek.potocek@post.cz>
2023-07-27 19:52:20 +0200

    Fix black screen after context recreation (introduced in d5de2b5d)

--- fish-fillets/src/main/cpp/app/subsystem/graphics/texturetarget.cpp
+++ fish-fillets/src/main/cpp/app/subsystem/graphics/texturetarget.cpp
@@ -14,8 +14,7 @@ void TextureTarget::bind() {
 void TextureTarget::resize(unsigned width, unsigned height, float scale) { // TODO log, phys?
     m_size = {width, height};
     auto texSize = (scale * m_size).round();
-    if(texSize != m_texture.physSize()) {
+    if(texSize != m_texture.physSize())
         m_texture = Texture(m_system, m_size, texSize);
-        m_framebuffer.texture(m_texture.native());
-    }
+    m_framebuffer.texture(m_texture.native());
 }
