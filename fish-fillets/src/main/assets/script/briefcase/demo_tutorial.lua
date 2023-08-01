vasekp/fish-fillets

commit f1631ffb8a6aecfbf031fddcdef359b4fe7cd338

Rename and split Lua files for 'briefcase'

fish-fillets/src/main/assets/script/briefcase/demo_tutorial.lua
@@ -28,6 +28,8 @@ local function planPushingAnim(delay)
     end)
 end
 
+file_include("script/"..codename.."/tutorial_dialogs.lua")
+
 demo_enter()
 addm(0, "help1") -- Teď na nic nesahej
 planMoveAction(3, function() return level_action_showMoves("llll") end)

fish-fillets/src/main/assets/script/briefcase/dialogs.lua
@@ -57,96 +57,3 @@ dialog_add("kuf-m-nezvednu", "small", {
 dialog_add("kuf-m-kousek", "small", {
   cs="Ještě kousek, ať pod tebou můžu projet.",
   en="Just a little bit higher so I can swim underneath you."})
-
-dialog_add("help1", "small", {
-  cs="Teď na nic nesahej, jen se dívej. Ukážeme ti, co bys s námi neměl dělat a co dělat umíme.",
-  en="For now, don’t touch anything, just watch and learn. We’ll show you what you should and shouldn’t do with us as well as what things we’re capable of."})
-
-dialog_add("help2", "big", {
-  cs="Než vstoupíme do dílny, uložíme si pozici - dělá se to klávesou F2.",
-  en="Before entering the workshop, let’s save the game - just press the F2 key."})
-
-dialog_add("help3", "small", {
-  cs="Nejprve ti ukážeme, co nám ubližuje.",
-  en="First we’ll show you what can hurt us."})
-
-dialog_add("help4", "big", {
-  cs="Já budu dělat figuranta.",
-  en="I’ll volunteer to be the dummy."})
-
-dialog_add("help5", "small", {
-  cs="Především bychom na sebe neměli házet předměty.",
-  en="First of all, we shouldn’t drop things on each other."})
-
-dialog_add("help6", "small", {
-  cs="Stejně tak nesmíme podklesnout pod předmětem, který neseme - spadnul by na nás.",
-  en="We also can’t swim downwards when carrying an object, because it would fall on us."})
-
-dialog_add("help7", "big", {
-  cs="Nyní začínáme znovu - můžeme však nahrát uloženou pozici klávesou F3.",
-  en="Now we’ll start again - or we can load the saved game by pressing the F3 key."})
-
-dialog_add("help8", "big", {
-  cs="Tak, a jsme na místě, kde jsme hru ukládali.",
-  en="And now we’re back where we last saved the game."})
-
-dialog_add("help9", "small", {
-  cs="Další věc, kterou nesmíme dělat, je posunovat si předmět po hřbetě.",
-  en="Another thing we must not do is push objects along our back."})
-
-dialog_add("help10", "small", {
-  cs="Některé předměty mají takový tvar, že bych je mohla zároveň držet i posouvat - ale to také nesmím.",
-  en="Some objects are shaped in such a way that we could hold them and push them at the same time - but we’re not allowed to do this either."})
-
-dialog_add("help11", "big", {
-  cs="Znovu nahrajeme pozici klávesou F3.",
-  en="Again, we load a saved game by pressing the F3 key."})
-
-dialog_add("help12", "small", {
-  cs="Nesmíme ani přihazovat předměty na hromádku, kterou někdo z nás drží.",
-  en="We also can’t drop additional objects on a pile that one of us is carrying."})
-
-dialog_add("help13", "small", {
-  cs="A já, na rozdíl od většího kamaráda, nejen že nepohnu s ocelovými předměty, ale nesmím je ani držet.",
-  en="And unlike my bigger partner, I can’t move or even carry steel objects."})
-
-dialog_add("help14", "big", {
-  cs="Naposledy jsme nahráli pozici. Teď ti ukážeme, co dělat umíme.",
-  en="We have loaded our saved game for the last time. Now we’ll show you all the things we’re capable of."})
-
-dialog_add("help15", "small", {
-  cs="Předmět můžeme vždy zvedat a zase pouštět.",
-  en="We can always pick up objects and then let them fall."})
-
-dialog_add("help16", "small", {
-  cs="Pokud se těsně před tím, než by nás měl zamáčknout, někde zachytí, můžeme ho i posouvat jedna po druhé...",
-  en="We can push an object along each other’s back only if the object will then be pushed onto another support..."})
-
-dialog_add("help17", "small", {
-  cs="... a dokonce ho na sebe pouštět.",
-  en="... and even let it drop on each other in this case."})
-
-dialog_add("help18", "small", {
-  cs="To mohu udělat i sama... podklesnu pod ním... a nebo jej nasunu támhle na tu polici.",
-  en="I can even do this alone... I can swim down under it... or put it on this ledge."})
-
-dialog_add("help19", "small", {
-  cs="Také můžeme posouvat předmět po jiném předmětu, který držíme, ne však přímo po zádech.",
-  en="We can also push one object along the top of another object which we are supporting, but we can’t move the object that is directly on our back."})
-
-dialog_add("help20", "big", {
-  cs="Můžeme také pod předmětem volně proplouvat a předávat si jej.",
-  en="We can also swim freely under an object which we are supporting and pass it to the other fish."})
-
-dialog_add("help21", "small", {
-  cs="A pokud předmět hned spadne, můžeme ho jedna z druhé shodit.",
-  en="And we can push objects off of each other’s back, as long as the object falls down immediately."})
-
-dialog_add("help22", "big", {
-  cs="Tak, to by asi bylo z pravidel všechno.",
-  en="That’s about it for the rules."})
-
-dialog_add("help23", "small", {
-  cs="Já bych to ještě shrnula. Předměty můžeme beztrestně pouze zvedat, pouštět, posouvat po pevné podložce, na něco je nasouvat nebo je ze sebe shazovat.",
-  en="In summary, we can only lift objects, let them drop, push them along a solid surface, push them onto a surface, and push them off of each other."})
-

fish-fillets/src/main/assets/script/briefcase/code.lua
@@ -20,7 +20,7 @@ local function prog_init()
         return function()
             if isReady(small) and isReady(big) then
                 if not demo_state() and small.X == 25 and small.Y == 23 and big.X == 27 and big.Y == 21 then
-                    file_include("script/"..codename.."/demo_help.lua")
+                    file_include("script/"..codename.."/demo_tutorial.lua")
                 end
                 if not demo_state() and no_dialog() then
                     if room.uvod == 0 then

fish-fillets/src/main/assets/script/briefcase/demo_briefcase.lua
@@ -1,6 +1,5 @@
 
-codename = "briefcase"
-file_include("script/briefcase/brief_dialogs.lua")
+file_include("script/"..codename.."/brief_dialogs.lua")
 
 local function planDelay(delay)
     if delay > 0 then

fish-fillets/src/main/assets/script/briefcase/tutorial_dialogs.lua
@@ -1,0 +1,92 @@
+dialog_add("help1", "small", {
+  cs="Teď na nic nesahej, jen se dívej. Ukážeme ti, co bys s námi neměl dělat a co dělat umíme.",
+  en="For now, don’t touch anything, just watch and learn. We’ll show you what you should and shouldn’t do with us as well as what things we’re capable of."})
+
+dialog_add("help2", "big", {
+  cs="Než vstoupíme do dílny, uložíme si pozici - dělá se to klávesou F2.",
+  en="Before entering the workshop, let’s save the game - just press the F2 key."})
+
+dialog_add("help3", "small", {
+  cs="Nejprve ti ukážeme, co nám ubližuje.",
+  en="First we’ll show you what can hurt us."})
+
+dialog_add("help4", "big", {
+  cs="Já budu dělat figuranta.",
+  en="I’ll volunteer to be the dummy."})
+
+dialog_add("help5", "small", {
+  cs="Především bychom na sebe neměli házet předměty.",
+  en="First of all, we shouldn’t drop things on each other."})
+
+dialog_add("help6", "small", {
+  cs="Stejně tak nesmíme podklesnout pod předmětem, který neseme - spadnul by na nás.",
+  en="We also can’t swim downwards when carrying an object, because it would fall on us."})
+
+dialog_add("help7", "big", {
+  cs="Nyní začínáme znovu - můžeme však nahrát uloženou pozici klávesou F3.",
+  en="Now we’ll start again - or we can load the saved game by pressing the F3 key."})
+
+dialog_add("help8", "big", {
+  cs="Tak, a jsme na místě, kde jsme hru ukládali.",
+  en="And now we’re back where we last saved the game."})
+
+dialog_add("help9", "small", {
+  cs="Další věc, kterou nesmíme dělat, je posunovat si předmět po hřbetě.",
+  en="Another thing we must not do is push objects along our back."})
+
+dialog_add("help10", "small", {
+  cs="Některé předměty mají takový tvar, že bych je mohla zároveň držet i posouvat - ale to také nesmím.",
+  en="Some objects are shaped in such a way that we could hold them and push them at the same time - but we’re not allowed to do this either."})
+
+dialog_add("help11", "big", {
+  cs="Znovu nahrajeme pozici klávesou F3.",
+  en="Again, we load a saved game by pressing the F3 key."})
+
+dialog_add("help12", "small", {
+  cs="Nesmíme ani přihazovat předměty na hromádku, kterou někdo z nás drží.",
+  en="We also can’t drop additional objects on a pile that one of us is carrying."})
+
+dialog_add("help13", "small", {
+  cs="A já, na rozdíl od většího kamaráda, nejen že nepohnu s ocelovými předměty, ale nesmím je ani držet.",
+  en="And unlike my bigger partner, I can’t move or even carry steel objects."})
+
+dialog_add("help14", "big", {
+  cs="Naposledy jsme nahráli pozici. Teď ti ukážeme, co dělat umíme.",
+  en="We have loaded our saved game for the last time. Now we’ll show you all the things we’re capable of."})
+
+dialog_add("help15", "small", {
+  cs="Předmět můžeme vždy zvedat a zase pouštět.",
+  en="We can always pick up objects and then let them fall."})
+
+dialog_add("help16", "small", {
+  cs="Pokud se těsně před tím, než by nás měl zamáčknout, někde zachytí, můžeme ho i posouvat jedna po druhé...",
+  en="We can push an object along each other’s back only if the object will then be pushed onto another support..."})
+
+dialog_add("help17", "small", {
+  cs="... a dokonce ho na sebe pouštět.",
+  en="... and even let it drop on each other in this case."})
+
+dialog_add("help18", "small", {
+  cs="To mohu udělat i sama... podklesnu pod ním... a nebo jej nasunu támhle na tu polici.",
+  en="I can even do this alone... I can swim down under it... or put it on this ledge."})
+
+dialog_add("help19", "small", {
+  cs="Také můžeme posouvat předmět po jiném předmětu, který držíme, ne však přímo po zádech.",
+  en="We can also push one object along the top of another object which we are supporting, but we can’t move the object that is directly on our back."})
+
+dialog_add("help20", "big", {
+  cs="Můžeme také pod předmětem volně proplouvat a předávat si jej.",
+  en="We can also swim freely under an object which we are supporting and pass it to the other fish."})
+
+dialog_add("help21", "small", {
+  cs="A pokud předmět hned spadne, můžeme ho jedna z druhé shodit.",
+  en="And we can push objects off of each other’s back, as long as the object falls down immediately."})
+
+dialog_add("help22", "big", {
+  cs="Tak, to by asi bylo z pravidel všechno.",
+  en="That’s about it for the rules."})
+
+dialog_add("help23", "small", {
+  cs="Já bych to ještě shrnula. Předměty můžeme beztrestně pouze zvedat, pouštět, posouvat po pevné podložce, na něco je nasouvat nebo je ze sebe shazovat.",
+  en="In summary, we can only lift objects, let them drop, push them along a solid surface, push them onto a surface, and push them off of each other."})
+

commit f829fff9e8eb8842214e02536b0b936b78f63cc3

Fix 'briefcase' tutorial not reloading dialogs on restart (broken by f1631ffb8)

fish-fillets/src/main/assets/script/briefcase/demo_tutorial.lua
@@ -28,8 +28,6 @@ local function planPushingAnim(delay)
     end)
 end
 
-file_include("script/"..codename.."/tutorial_dialogs.lua")
-
 demo_enter()
 addm(0, "help1") -- Teď na nic nesahej
 planMoveAction(3, function() return level_action_showMoves("llll") end)

fish-fillets/src/main/assets/script/briefcase/dialogs.lua
@@ -57,3 +57,5 @@ dialog_add("kuf-m-nezvednu", "small", {
 dialog_add("kuf-m-kousek", "small", {
   cs="Ještě kousek, ať pod tebou můžu projet.",
   en="Just a little bit higher so I can swim underneath you."})
+
+file_include("script/"..codename.."/tutorial_dialogs.lua")