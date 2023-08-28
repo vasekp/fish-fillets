d5dc2fd221a4fe5558e30d52a142d88ec69dcec8
Vašek Potoček <vasek.potocek@post.cz>
2023-08-28 11:30:40 +0200

    Linux: FreeType caching (own implementation)

--- fish-fillets/src/main/cpp/app/platform/linux/ft-font.cpp
+++ fish-fillets/src/main/cpp/app/platform/linux/ft-font.cpp
@@ -11,8 +11,13 @@ static float from266(FT_F26Dot6 x) {
     return (float)x / 64.f;
 }
 
+static float from1616(FT_F26Dot6 x) {
+    return (float)x / 65536.f;
+}
+
 FTFont::FTFont(Instance& instance, const std::string& filename) :
-    IFont(instance)
+    IFont(instance),
+    m_stroker()
 {
     auto fnFull = dynamic_cast<SystemFile&>(*instance.files().system(filename)).fullPath(); // TODO
     if(FT_Init_FreeType(&m_ft) != 0)
@@ -20,6 +25,15 @@ FTFont::FTFont(Instance& instance, const std::string& filename) :
 
     if(FT_New_Face(m_ft, fnFull.c_str(), 0, &m_face) != 0)
         Log::fatal("Error loading font ", fnFull);
+
+    FT_Stroker_New(m_ft, &m_stroker);
+}
+
+FTFont::~FTFont() {
+    m_cache.clear();
+    FT_Stroker_Done(m_stroker);
+    FT_Done_Face(m_face);
+    FT_Done_FreeType(m_ft);
 }
 
 void FTFont::own_setSizes() {
@@ -28,6 +42,39 @@ void FTFont::own_setSizes() {
     Log::debug<Log::graphics>("EM ", m_face->size->metrics.y_ppem,
             " asc ", from266(m_face->size->metrics.ascender),
             " desc ", -from266(m_face->size->metrics.descender));
+    if(m_outline)
+        FT_Stroker_Set(m_stroker, to266(m_outline), FT_STROKER_LINECAP_ROUND, FT_STROKER_LINEJOIN_ROUND, 0);
+    m_cache.clear();
+}
+
+std::pair<FT_Glyph, FT_Glyph> FTFont::getGlyphs(wchar_t c) const {
+    if(auto it = m_cache.find(c); it != m_cache.end()) {
+        return {it->second.first.get(), it->second.second.get()};
+    }
+    auto mgdGlyph = [](FT_Glyph g) {
+        return ManagedGlyph{g, &FT_Done_Glyph};
+    };
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter{};
+    if(FT_Load_Char(m_face, c, FT_LOAD_DEFAULT) != 0) {
+        Log::error("Can't load character ", converter.to_bytes(c));
+        m_cache.insert_or_assign(c, std::pair{mgdGlyph(nullptr), mgdGlyph(nullptr)});
+        return {nullptr, nullptr};
+    }
+    FT_Glyph glyph, outGlyph = nullptr;
+    FT_Vector vector{};
+    if(FT_Get_Glyph(m_face->glyph, &glyph) != 0)
+        Log::error("FT_Get_Glyph");
+    if(m_outline) {
+        outGlyph = glyph;
+        if(FT_Glyph_StrokeBorder(&outGlyph, m_stroker, 0, 0) != 0)
+            Log::error("FT_Glyph_StrokeBorder");
+        if(FT_Glyph_To_Bitmap(&outGlyph, FT_RENDER_MODE_NORMAL, &vector, 1) != 0)
+            Log::error("FT_Glyph_To_Bitmap");
+    }
+    if(FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, &vector, 1) != 0)
+        Log::error("FT_Glyph_To_Bitmap");
+    auto it = m_cache.insert_or_assign(c, std::pair{mgdGlyph(glyph), mgdGlyph(outGlyph)}).first;
+    return {it->second.first.get(), it->second.second.get()};
 }
 
 std::vector<std::string> FTFont::own_breakLines(const std::string& text, float width) {
@@ -43,12 +90,10 @@ std::vector<std::string> FTFont::own_breakLines(const std::string& text, float w
 
     for(auto i = 0u; i < wtext.size(); i++) {
         auto c = wtext[i];
-        if(FT_Load_Char(m_face, wtext[i], 0) != 0) {
-            Log::error("Can't load character ", converter.to_bytes(wtext[i]));
+        auto glyph = getGlyphs(c).first;
+        if(!glyph)
             continue;
-        }
-        auto slot = m_face->glyph;
-        fwidth += from266(slot->advance.x);
+        fwidth += from1616(glyph->advance.x);
         if(c == L' ' || c == L'\n') {
             lastBreak = i;
             lastBreakX = fwidth;
@@ -78,12 +123,9 @@ ImageData FTFont::own_renderText(const std::string& text) const {
     float fwidth = 0.f;
 
     for(auto c : wtext) {
-        if(FT_Load_Char(m_face, c, 0) != 0) {
-            Log::error("Can't load character ", converter.to_bytes(c));
-            continue;
-        }
-        auto slot = m_face->glyph;
-        fwidth += from266(slot->advance.x);
+        auto glyph = getGlyphs(c).first;
+        if(glyph)
+            fwidth += from1616(glyph->advance.x);
     }
 
     auto width = (unsigned)(fwidth + 2.f * m_outline) + 2;
@@ -107,46 +149,33 @@ ImageData FTFont::own_renderText(const std::string& text) const {
     FCoords origin{m_outline + 1.f, m_outline + asc + 1.f};
     if(m_outline) {
         auto pen = origin;
-        FT_Stroker stroker;
-        FT_Stroker_New(m_ft, &stroker);
-        FT_Stroker_Set(stroker, to266(m_outline), FT_STROKER_LINECAP_ROUND, FT_STROKER_LINEJOIN_ROUND, 0);
-
         for(auto c : wtext) {
-            if(FT_Load_Char(m_face, c, 0) != 0) {
-                Log::error("Can't load character ", converter.to_bytes(c));
+            auto glyph = getGlyphs(c).second;
+            if(!glyph)
                 continue;
-            }
-            auto slot = m_face->glyph;
-            FT_Glyph glyph;
-            if(FT_Get_Glyph(slot, &glyph) != 0)
-                Log::error("FT_Get_Glyph");
-            if(FT_Glyph_StrokeBorder(&glyph, stroker, 0, 1) != 0)
-                Log::error("FT_Glyph_StrokeBorder");
-            FT_Vector vector{};
-            if(FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, &vector, 1) != 0)
-                Log::error("FT_Glyph_To_Bitmap");
-            auto bmpGlyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
+            auto bmp = reinterpret_cast<FT_BitmapGlyph>(glyph);
             Log::verbose<Log::graphics>("glyph ", converter.to_bytes(c),
-                    " at ", pen, " ", bmpGlyph->left, " ", bmpGlyph->top,
-                    " ", bmpGlyph->bitmap.width, "x", bmpGlyph->bitmap.rows,
-                    " advance ", from266(slot->advance.x), ", ", from266(slot->advance.y));
-            blend(bmpGlyph->bitmap, (int)pen.x + bmpGlyph->left, (int)pen.y - bmpGlyph->top, false);
-            FT_Done_Glyph(glyph);
-            pen += FCoords{from266(slot->advance.x), from266(slot->advance.y)};
+                    " at ", pen, " ", bmp->left, " ", bmp->top,
+                    " ", bmp->bitmap.width, "x", bmp->bitmap.rows,
+                    " advance ", from1616(glyph->advance.x), ", ", from1616(glyph->advance.y));
+            blend(bmp->bitmap, (int)pen.x + bmp->left, (int)pen.y - bmp->top, false);
+            pen += FCoords{from1616(glyph->advance.x), from1616(glyph->advance.y)};
         }
-        FT_Stroker_Done(stroker);
     }
 
     {
         auto pen = origin;
         for(auto c : wtext) {
-            if(FT_Load_Char(m_face, c, FT_LOAD_RENDER) != 0) {
-                Log::error("Can't load character ", converter.to_bytes(c));
+            auto glyph = getGlyphs(c).first;
+            if(!glyph)
                 continue;
-            }
-            auto slot = m_face->glyph;
-            blend(slot->bitmap, (int)pen.x + slot->bitmap_left, (int)pen.y - slot->bitmap_top, true);
-            pen += FCoords{from266(slot->advance.x), from266(slot->advance.y)};
+            auto bmp = reinterpret_cast<FT_BitmapGlyph>(glyph);
+            Log::verbose<Log::graphics>("glyph ", converter.to_bytes(c),
+                    " at ", pen, " ", bmp->left, " ", bmp->top,
+                    " ", bmp->bitmap.width, "x", bmp->bitmap.rows,
+                    " advance ", from1616(glyph->advance.x), ", ", from1616(glyph->advance.y));
+            blend(bmp->bitmap, (int)pen.x + bmp->left, (int)pen.y - bmp->top, true);
+            pen += FCoords{from1616(glyph->advance.x), from1616(glyph->advance.y)};
         }
     }
 
--- fish-fillets/src/main/cpp/app/platform/linux/ft-font.h
+++ fish-fillets/src/main/cpp/app/platform/linux/ft-font.h
@@ -10,14 +10,19 @@
 class FTFont : public IFont {
 public:
     FTFont(Instance& instance, const std::string& filename);
+    ~FTFont();
 
 private:
     FT_Library m_ft;
     FT_Face m_face;
+    FT_Stroker m_stroker;
+    using ManagedGlyph = std::unique_ptr<FT_GlyphRec, void(*)(FT_GlyphRec*)>;
+    mutable std::map<wchar_t, std::pair<ManagedGlyph, ManagedGlyph>> m_cache;
 
     void own_setSizes() override;
     std::vector<std::string> own_breakLines(const std::string& text, float width) override;
     ImageData own_renderText(const std::string& text) const override;
+    std::pair<FT_Glyph, FT_Glyph> getGlyphs(wchar_t c) const;
 };
 
 namespace decoders {
