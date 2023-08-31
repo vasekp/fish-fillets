dbdd7fb2d5c421f8b2a039572ffdbc4124853145
Vašek Potoček <vasek.potocek@gmail.com>
2023-08-31 23:18:24 +0200

    iOS: first commit

---
+++ fish-fillets/src/main/swift/filletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  filletsApp.swift
+//  fillets
+//
+//  Created by vasek on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct filletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
